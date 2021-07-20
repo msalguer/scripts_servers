33152ffe5324bc649a149a6001bb91d4a3fffa71
Manuel Salguero <msalguer@hotmail.com>
2021-07-20 20:47:57 +0200

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -18,16 +18,6 @@ Cyan='\033[0;36m'         # Cyan
 echo -e "$Cyan \n Updating System.. $Color_Off"
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-echo -e "$Yellow \n Do you use Gnome desktop? (y/N): $Color_Off"
-read -e gnome
-
-if [ "$gnome" == "y" || "$gnome" == "Y"] 
-then
-    #Nothing
-else
-        sudo apt install gnome-session gdm3 -y
-fi
-
 sudo apt-get install xrdp -y
 sudo ufw allow 3389/tcp
 cat > /etc/polkit-1/localauthority.conf.d/02-allow-colord.conf << EOF
@@ -39,4 +29,16 @@ return polkit.Result.YES;
 });
 EOF
 sudo /etc/init.d/xrdp restart
-echo -e "$Green \n Installation finished. $Color_Off"
+
+echo -e "$Yellow \n Do you use Gnome desktop? (y/N): $Color_Off"
+read gnome
+
+if [ "$gnome" == "y" || "$gnome" == "Y"] 
+then
+        echo -e "$Yellow \n On Gnome Desktop, anything is necessary. $Color_Off"
+else
+        echo -e "$Yellow \n If when finished installation, consider install the next line,only if not works:"
+        echo "sudo apt install gnome-session gdm3 -y $Color_Off"
+fi
+
+echo -e "$Green \n Installation finished. Try connect with RDP client on 3389 port. $Color_Off"
